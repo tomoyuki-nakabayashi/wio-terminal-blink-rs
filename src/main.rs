4cb2a9e77df05e012980fc95ba87f6a490255d4f
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2020-12-24 06:29:25 +0900

    prepare blink example.

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+#![no_std]
+#![no_main]
+
+//! Wio Terminal のユーザー LED を点滅するサンプルプログラムです。
+
+use panic_halt as _;
+use wio_terminal as wio;
+
+use wio::hal::clock::GenericClockController;
+use wio::hal::delay::Delay;
+use wio::pac::{CorePeripherals, Peripherals};
+use wio::prelude::*;
+use wio::{entry, Pins, Sets};
+
+#[entry]
+fn main() -> ! {
+    let mut peripherals = Peripherals::take().unwrap();
+    let core = CorePeripherals::take().unwrap();
+
+    let mut clocks = GenericClockController::with_external_32kosc(
+        peripherals.GCLK,
+        &mut peripherals.MCLK,
+        &mut peripherals.OSC32KCTRL,
+        &mut peripherals.OSCCTRL,
+        &mut peripherals.NVMCTRL,
+    );
+    let mut delay = Delay::new(core.SYST, &mut clocks);
+
+    let mut sets: Sets = Pins::new(peripherals.PORT).split();
+    let mut user_led = sets.user_led.into_push_pull_output(&mut sets.port);
+
+    loop {
+        user_led.toggle();
+        delay.delay_ms(500u16);
+    }
+}
